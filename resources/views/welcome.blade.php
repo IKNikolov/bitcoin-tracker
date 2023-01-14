46eaea6c152e069ac98c1f75562b419e18205538
Ivaylo Nikolov <inikolov@sonicomit.com>
2023-01-14 13:06:40 +0200

    Installed Laravel 8 and vue UI

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bitcoin tracker</title>
+
+    </head>
+    <body>
+        <div id="app">
+            <example-component></example-component>
+         </div>
+        <script src="{{ mix('/js/app.js') }}"></script>
+    </body>
+</html>
